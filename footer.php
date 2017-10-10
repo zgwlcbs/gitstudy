a102ec12b15b3e277e9da893fc8998c5a27ab0e5
red <whatwhywhen123@163.com>
2017-10-10 18:11:24 +0800

    footerfooterfooterfooterfooter

--- footer.php
+++ footer.php
@@ -3,48 +3,46 @@
 		</div><!--end of page container-->
 		<div class="footer">
 			<div class="container-fluid">
-				<div class="footer-widget">
-					<div class="row">
+					<div class="row footer-widget">
 						<div class="col-md-4">
 							<div class="row wrapper">
 								<div class="col-md-5 wrapper">
 									<ul class="vertical-ul">
-										<li><a href="#">文章1<a></li>
-										<li><a href="#">文章1<a></li>
-										<li><a href="#">文章1<a></li>
-										<li><a href="#">文章1<a></li>
+										<li><a class="white-font" href="#">文章1<a></li>
+										<li><a class="white-font"  href="#">文章1<a></li>
+										<li><a class="white-font"  href="#">文章1<a></li>
+										<li><a class="white-font"  href="#">文章1<a></li>
 									</ul>
 								</div>
 								<div class="col-md-5 wrapper">
 									<ul class="vertical-ul white-font">
-										<li><a href="#">文章1<a></li>
-										<li><a href="#">文章1<a></li>
-										<li><a href="#">文章1<a></li>
-										<li><a href="#">文章1<a></li>
+										<li><a class="white-font"  href="#">文章1<a></li>
+										<li><a class="white-font"  href="#">文章1<a></li>
+										<li><a class="white-font"  href="#">文章1<a></li>
+										<li><a class="white-font"  href="#">文章1<a></li>
 									</ul>
 								</div>
 							</div>
 						</div>
-						<div class="col-md-5">
+						<div class="col-md-5 white-font">
 							<h3>关于我们</h3>
 							<p>关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们关于我们</p>
 						</div>
 						<div class="col-md-3">
-							<h3>关注我们</h3>
+							<h3 class="white-font">关注我们</h3>
 							<ul class="horizontal-ul">
-								<li><a title="微信">微信</a></li>
-								<li><a title="微博">微博</a></li>
+								<li><a class="white-font"  title="微信">微信</a></li>
+								<li><a class="white-font"  title="微博">微博</a></li>
 							</ul>
 						</div>
-					</div>
-				</div>
+				</div><!--end of footer-widget row-->
 				<div class="row">
 					<div class="col-md-12 col-sm-12 copyright">
 						Copyright ©2017
 						<a href="<?php echo get_option('home'); ?>"><?php bloginfo('name'); ?></a>
 					</div>
-				</div>
-			</div><!--end of footer container-->
+				</div><!--end of copyright row-->
+			</div><!--end of footer container-fluid-->
 		</div><!--end of footer-->
 		<?php wp_footer(); ?>
 		<script type="javascript" src="<?php bloginfo('template_directory'); ?>/js/bootstrap.js">
